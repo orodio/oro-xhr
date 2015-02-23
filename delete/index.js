368e6a5d72829bfd48254cd2b2312ab76de21822
James Bjorkman <james@bjorkman.ca>
2015-02-24 09:21:11 +1100

    V2.0.0

---
+++ delete/index.js
@@ -0,0 +1,13 @@
+import { Promise } from "es6-promise"
+import xhr         from "superagent"
+import response    from "../lib/response"
+
+export default function(url, data, parse=true) {
+  return new Promise(function(resolve, reject) {
+    let _xhr = xhr.del(url);
+
+    if (data) _xhr.send(data);
+
+    _xhr.end(response(resolve, reject, parse));
+  });
+}
